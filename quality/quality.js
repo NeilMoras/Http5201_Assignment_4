f64b71a1ca1a6ba1ff51a192e6a656ed3015f579
Neil Moras <neilmoras@gmail.com>
2021-08-09 12:13:54 -0400

    first commit

---
+++ quality/quality.js
@@ -0,0 +1,26 @@
+// import '../style.scss'
+//
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
+//
+//
+// document.querySelector('#header').innerHTML = `
+// <div class="text-wrapper">
+// <div class="nav-flex">
+// <div class="logo">
+//   <a href="index.html"><img id="web-logo" src="images/fake-logo.png" alt="Logo of the website" /></a>
+// </div>
+// <nav id="menu">
+//   <h2 class="hidden">Header Navigation Menu</h2>
+//   <ul>
+//     <li><a href="security/index.html" target="_blank">Security</a></li>
+//     <li><a href="quality/index.html" target="_blank">Quality</a></li>
+//     <li><a href="badexample/index.html" target="_blank">Bad Example</a></li>
+//   </ul>
+// </nav>
+// </div>
+//
+// </div>
+`
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+const { resolve } = require('path')
+
+module.exports = {
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        security: resolve(__dirname, 'security/index.html'),
+        quality: resolve(__dirname, 'quality/index.html'),
+          badexample: resolve(__dirname, 'badexample/index.html')  
+      }
+    }
+  }
+}
